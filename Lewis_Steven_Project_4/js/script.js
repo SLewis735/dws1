d7663785d0641c0b4e15561891f71006d38d8c52
SLewis735 <selewis@fullsail.edu>
2015-05-28 13:41:57 -0400

    js

---
+++ Lewis_Steven_Project_4/js/script.js
@@ -0,0 +1,11 @@
+alert("Welcome to Slab's Rib Shack")
+// var goodFood = prompt("Do you love Down Home Southern Cooking");
+var goodFood = prompt("How often do you get good cooking a week");
+var tryStabs = 
+
+
+if(goodFood < 10){
+	prompt("Sorry that you are not eating well, Come down to Stabs for some good cooking");
+}else{
+	prompt("Kiss your momma or your cook for the good cooking");
+}

99a11c0938319886992af4f52def2e88e0ceb619
SLewis735 <selewis@fullsail.edu>
2015-06-25 12:02:03 -0400

    js

---
+++ Lewis_Steven_Project_4/js/script.js
@@ -0,0 +1,17 @@
+alert("It's OK to be a Quitter!");
+
+alert("What is the cost of smoking?");
+
+var packs = prompt("How many packs of cigarettes do you smoke a day?");
+var days = prompt("How many days a week do you smoke?");
+var years = prompt("How many years have you smoke?");
+
+var cost = $5.51(packs * days * years) = alert("This is the average amount you spend on cigarettes in your lifetime?");
+
+//if( < 10){
+//	alert("Sorry that you are not eating well, Come down to Stabs for some good cooking!!!");
+//}else{
+//    alert("Congrats!!");
+//	alert("Kiss your momma or your cook that loves you for the good cooking.");
+//}
+//alert("Enjoy the Website and have a happy finger licking day.");

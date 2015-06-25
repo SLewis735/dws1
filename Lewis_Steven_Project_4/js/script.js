37e1de05f03b8db35f329e1983dbaf913c3e7d8b
SLewis735 <selewis@fullsail.edu>
2015-06-25 13:02:35 -0400

    o

--- Lewis_Steven_Project_4/js/script.js
+++ Lewis_Steven_Project_4/js/script.js
@@ -6,13 +6,15 @@ var packs = prompt("How many packs of cigarettes do you smoke a day?");
 var days = prompt("How many days a week do you smoke?");
 var years = prompt("How many years have you smoke?");
 cost = 5.51
+society = 18.01
 total = packs * days * years * cost;
-var smoking = "This is the average amount you spend on cigarettes in your lifetime is " + '$'total;
+grand = packs * days * years * society;
+alert("The average retail price of a pack of cigarettes in America is $5.51.");
+alert("But the real price of a pack of cigarettes to society and to the state's economy is $18.05 per pack.");
+var smoking = "This is the average amount you spend on cigarettes in your lifetime is " + total;
+var economy = "Your contribution to society and to the state's economy for a lifetime of smoking is " + economy;
 alert(smoking);
-//if( < 10){
-//	alert("Sorry that you are not eating well, Come down to Stabs for some good cooking!!!");
-//}else{
-//    alert("Congrats!!");
-//	alert("Kiss your momma or your cook that loves you for the good cooking.");
-//}
-//alert("Enjoy the Website and have a happy finger licking day.");
+alert(economy);
+
+
+

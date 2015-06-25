ac5c54fce2da2fd0d4af0b2349b958f91409ded2
SLewis735 <selewis@fullsail.edu>
2015-06-25 12:19:11 -0400

    p

--- Lewis_Steven_Project_4/js/script.js
+++ Lewis_Steven_Project_4/js/script.js
@@ -6,7 +6,7 @@ var packs = prompt("How many packs of cigarettes do you smoke a day?");
 var days = prompt("How many days a week do you smoke?");
 var years = prompt("How many years have you smoke?");
 
-var cost = 5.51 * packs * days * years = alert("This is the average amount you spend on cigarettes in your lifetime?");
+var cost = 5.51(packs * days * years) = alert("This is the average amount you spend on cigarettes in your lifetime?");
 
 //if( < 10){
 //	alert("Sorry that you are not eating well, Come down to Stabs for some good cooking!!!");

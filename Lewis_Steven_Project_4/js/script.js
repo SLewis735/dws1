eca96e26a06ebe1e241aab5f2119f9aa7415981f
SLewis735 <selewis@fullsail.edu>
2015-06-25 13:16:19 -0400

    done

--- Lewis_Steven_Project_4/js/script.js
+++ Lewis_Steven_Project_4/js/script.js
@@ -16,6 +16,7 @@ var economy = "Your contribution to society and to the state's economy for a lif
 alert(smoking);
 alert(economy);
 if(smoking > 1000){alert("Just think of the vacations you could have enjoyed with the family or buying that new toys you had your eye on");
+}
 alert("The real cost of smoking is your health and life.");
 alert("Are you ready to be a Quitter?");
 
